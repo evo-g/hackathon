bf8442aee8fc613b7d02b5debe0bf161f002a465
Evodio Garcia <evodiog@gmail.com>
2018-07-10 02:58:25 -0700

    make it better styled

--- src/App.js
+++ src/App.js
@@ -49,13 +49,19 @@ export default class Hackathon extends Component {
 
   render() {
     return (
-      <div className='container'>
-        <div>
+      <div className='container-fluid'>
+        <div className='jumbotron'>
           <h1 className='header text-center'> Basic Movie Search </h1>
+          <p className='text-center'> Search your favorite movies</p>
+        </div>
+
+        <div className='row'>
+
+        <div className='col-lg-12'>
           <input className="w-75 search" value={this.state.text} onChange={this.handleTextChange} placeholder='search movies here...' />
-          <button className='btn btn-success btn-center search-movies w-25 glyphicon glyphicon-search' onClick={this.searchMovies}></button>
-          <div className='card'>
-            <h2 className='text-center text-warning card-header'> Movie Results </h2>
+          <button className='btn btn-success btn-center w-25 search-movies glyphicon glyphicon-search' onClick={this.searchMovies}></button>
+          <h2 className='text-center card-header w-100'> Movie Results: </h2>
+
             <ul>
               {this.state.movies.map((movie, index, array) => {
                 return (
@@ -66,8 +72,12 @@ export default class Hackathon extends Component {
               })}
             </ul>
           </div>
-        </div>
-      </div>
+          </div>
+          </div>
+
+
+
+
     );
   }
 }
@@ -104,8 +114,9 @@ class MovieResults extends Component {
   render() {
     return (
       (!this.state.isClicked) ?
-        <div className='col-sm-12'>
-          <div className='card'>
+        <div className='well'>
+
+          <div className='card ac'>
             <div className='media'>
               <li>
                 <div className='media-left'>
@@ -113,7 +124,7 @@ class MovieResults extends Component {
                 </div>
                 <div className='media-body'>
                   <div className='text-info'> Movie Title: {this.props.movie.Title} </div>
-                  <button href='#' className='btn btn-info btn-centered' onClick={this.findMovieIdCall}>Click for more info</button>
+                  <button href='#' className='btn btn-info btn-centered text-center' onClick={this.findMovieIdCall}>Click for more info</button>
                 </div>
               </li>
             </div>
@@ -121,14 +132,14 @@ class MovieResults extends Component {
         </div>
         :
         // (this.state.isClicked) ?
-          <div className='card'>
-            <h4 className="card-header text-center text-info text-bold"> Movie Title: {this.props.movie.Title} </h4>
+          <div className='card bc'>
+            <h2 className="header-bc text-center text-bold"> {this.props.movie.Title} </h2>
             <div className='media'>
               <div className='media-left'><br />
                 <img src={this.props.movie.Poster} alt="description" className='media-object img-rounded' /><br />
               </div>
               <div className='media-body'><br />
-                <div className='text-warning'> Year: {this.props.movie.Year}</div>
+                <div className='text-white'> Year: {this.props.movie.Year}</div>
                 <div className='text-primary'>Actors: {this.state.movie.Actors}</div>
                 <div className='text-danger'>Plot: {this.state.movie.Plot}</div>
                 <div className='text-success'>BoxOffice: {this.state.movie.BoxOffice}</div>
@@ -136,12 +147,12 @@ class MovieResults extends Component {
                 <div className='text-success'>Director: {this.state.movie.Director}</div>
                 <div className='text-danger'>Awards: {this.state.movie.Awards}</div>
                 <div className='text-primary'>Genre: {this.state.movie.Genre}</div>
-                <div className='text-warning'>Rated: {this.state.movie.Rated}</div>
+                <div className='text-white'>Rated: {this.state.movie.Rated}</div>
                 <div>Language: {this.state.movie.Language}</div>
                 <div className='text-info'>Production: {this.state.movie.Production}</div>
                 <div className='text-success'>Metascore: {this.state.movie.Metascore}</div>
                 <div>Writer: {this.state.movie.Writer}</div>
-                <div className='text-warning'>Released: {this.state.movie.Released}</div>
+                <div className='text-white'>Released: {this.state.movie.Released}</div>
                 <button href='#' className='btn btn-danger btn-centered' onClick={this.findMovieIdCall}>Less info</button>
               </div>
             </div>
